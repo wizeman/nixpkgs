wizeman/nixpkgs

commit 1f007556a39a0a17c7a01c63fb7b613b4452399f

big breaking change: renaming lib.getAttr to lib.attrByPath getAttr was ambiguous. It's also a builtin function
fix

git-svn-id: https://svn.nixos.org/repos/nix/nixpkgs/trunk@15692 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

pkgs/applications/graphics/hugin/default.nix
@@ -1,4 +1,5 @@
-{stdenv, fetchurl, panotools, cmake, wxGTK, libtiff, libpng, openexr, boost, pkgconfig, exiv2, gettext, ilmbase }:
+{stdenv, fetchurl, panotools, cmake, wxGTK, libtiff, libpng, openexr, boost,
+  pkgconfig, exiv2, gettext, ilmbase, enblendenfuse, autopanosiftc }:
 
 stdenv.mkDerivation {
   name = "hugin-0.7.0";
@@ -35,6 +36,10 @@ stdenv.mkDerivation {
     eval -- "cmake .. $cmakeFlags"
     set +x
     '';
+  postInstall = ''
+    ensureDir $out/nix-support
+    echo ${enblendenfuse} ${autopanosiftc} > $out/nix-support/$out/nix-support/propagated-user-env-packages
+  '';
 
   meta = {
     homepage = http://hugin.sourceforge.net/;

pkgs/applications/audio/snd/default.nix
@@ -4,7 +4,7 @@ let
   fetchurl = args.fetchurl;
   fullDepEntry = args.fullDepEntry;
 
-  version = lib.getAttr ["version"] "9.4" args; 
+  version = lib.attrByPath ["version"] "9.4" args; 
   buildInputs = with args; [gtk glib pkgconfig 
      libXpm gmp gettext libX11 fftw]
       ++ (lib.optional (args ? ruby) args.ruby)

pkgs/applications/editors/eclipse/default.nix
@@ -9,25 +9,29 @@
 # Why use a local copy? This way it's easier to use the update manager to get plugins :-)
 
 
-{fetchurl, stdenv, jdk, gtk, glib, libXtst, plugins ? []}:
+{fetchurl, stdenv, jdk, gtk, glib, libXtst, makeOverridable, plugins ? [], unzip}:
 
-let {
-  body =
+let eclipseFun = 
+  makeOverridable ({name, bindist} :
     stdenv.mkDerivation {
-      name = "eclipse-sdk-3.5M6";
+      inherit name;
       builder = ./builder.sh;
       src = bindist;
-      buildInputs = [];
+      buildInputs = [ unzip /* unzip required by eclipseCDT */ ];
       inherit jdk plugins;
       libraries = [gtk glib libXtst];
-   };
+   }); in
 
-  bindist = 
-    if (stdenv.system == "x86_64-linux") then fetchurl {
-      url = ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/eclipse/S-3.5M6-200903130100/eclipse-SDK-3.5M6-linux-gtk-x86_64.tar.gz;
-      sha256 = "10p4idp5rcdf7xqwfk3kvmjxhi8x1v835m0y4pn9q4nhfb5643pi";
-    } else fetchurl {
-      url = ftp://mirror.micromata.de/eclipse/eclipse/downloads/drops/S-3.5M6-200903130100/eclipse-SDK-3.5M6-linux-gtk.tar.gz;
-      sha256 = "1z8j26b632ydhqrmwgbcqgiq7f1a542jam06z2h62mcbqazrcyah";
-    };
-}
+  eclipseFun {
+    # you can override these settings usnig .override {...} 
+    name = "eclipse-sdk-3.5M6";
+
+    bindist = 
+      if (stdenv.system == "x86_64-linux") then fetchurl {
+        url = ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/eclipse/S-3.5M6-200903130100/eclipse-SDK-3.5M6-linux-gtk-x86_64.tar.gz;
+        sha256 = "10p4idp5rcdf7xqwfk3kvmjxhi8x1v835m0y4pn9q4nhfb5643pi";
+      } else fetchurl {
+        url = ftp://mirror.micromata.de/eclipse/eclipse/downloads/drops/S-3.5M6-200903130100/eclipse-SDK-3.5M6-linux-gtk.tar.gz;
+        sha256 = "1z8j26b632ydhqrmwgbcqgiq7f1a542jam06z2h62mcbqazrcyah";
+      };
+  }

pkgs/applications/editors/eclipse/plugins/spoofax/default.nix
@@ -1,6 +1,0 @@
-{stdenv, fetchurl}:
-
-fetchurl {
-  url = http://www.ii.uib.no/~karltk/spoofax/plugins/org.spoofax.editor_0.3.11.jar;
-  md5 = "c36941afcb0e538e16fafd594eae128e";
-}

pkgs/applications/graphics/xaos/default.nix
@@ -2,7 +2,7 @@ a :
 let 
   fetchurl = a.fetchurl;
 
-  version = a.lib.getAttr ["version"] "3.4" a; 
+  version = a.lib.attrByPath ["version"] "3.4" a; 
   buildInputs = with a; [
     aalib gsl libpng libX11 xproto libXext xextproto 
     libXt zlib gettext intltool perl

pkgs/applications/graphics/xscreensaver/default.nix
@@ -1,5 +1,5 @@
 args : with args; let localDefs = builderDefs.passthru.function (args // rec {
-                version = lib.getAttr ["version"] "5.07" args;
+                version = lib.attrByPath ["version"] "5.07" args;
 		src = /* put a fetchurl here */
 		fetchurl {
 		    url = "http://www.jwz.org/xscreensaver/xscreensaver-${version}.tar.gz";

pkgs/applications/networking/browsers/midori/default.nix
@@ -5,7 +5,7 @@ let
   doPatchShebangs = args.doPatchShebangs;
   makeManyWrappers = args.makeManyWrappers;
 
-  version = lib.getAttr ["version"] "0.0.21" args; 
+  version = lib.attrByPath ["version"] "0.0.21" args; 
   buildInputs = with args; [
     intltool python imagemagick gtk glib webkit libxml2 
     gtksourceview pkgconfig which gettext makeWrapper 

pkgs/applications/networking/browsers/mozilla-plugins/mozplugger/default.nix
@@ -2,7 +2,7 @@ a :
 let 
   fetchurl = a.fetchurl;
 
-  version = a.lib.getAttr ["version"] "1.12.0" a; 
+  version = a.lib.attrByPath ["version"] "1.12.0" a; 
   buildInputs = with a; [
     firefox libX11 xproto
   ];

commit 36af1c94aee2f581d15155e61f286a00916138a3

Hugin: Fix typo stealthily introduced in r15692.
git-svn-id: https://svn.nixos.org/repos/nix/nixpkgs/trunk@16332 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

pkgs/applications/graphics/hugin/default.nix
@@ -37,8 +37,8 @@ stdenv.mkDerivation {
     set +x
     '';
   postInstall = ''
-    ensureDir $out/nix-support
-    echo ${enblendenfuse} ${autopanosiftc} > $out/nix-support/$out/nix-support/propagated-user-env-packages
+    ensureDir "$out/nix-support"
+    echo "${enblendenfuse} ${autopanosiftc}" > $out/nix-support/propagated-user-env-packages
   '';
 
   meta = {