c4a4a615851bb74c60328aab4bf80e673c7f52be
Vladimír Čunát <vcunat@gmail.com>
2011-07-24 23:51:46 +0200

    xfce4-notifyd: icon cache deletion, linked the daemon to bin.

--- pkgs/desktops/xfce-4/applications/xfce4-notifyd.nix
+++ pkgs/desktops/xfce-4/applications/xfce4-notifyd.nix
@@ -1,14 +1,18 @@
-{ stdenv, fetchXfce #, pkgconfig, intltool, libexif
-, gtk
-#, exo, dbus_glib
-, libxfce4util, libxfce4ui, xfconf }:
+{ stdenv, fetchXfce, pkgconfig, intltool
+, gtk , libxfce4util, libxfce4ui, xfconf }:
 
 stdenv.mkDerivation rec {
   name = "xfce4-notifyd-0.2.1";
 
   src = fetchXfce.app name "0w7dqfzb7h6hg9n7bdnqwybakrz2p72ffi37h4fsj7j02hyxmcma";
 
-  buildInputs = [ gtk libxfce4util libxfce4ui xfconf ];
+  buildInputs = [ pkgconfig intltool gtk libxfce4util libxfce4ui xfconf ];
+
+  fixupPhase = ''
+    rm $out/share/icons/hicolor/icon-theme.cache
+    # to be able to run the daemon we need it in PATH
+    cp -l $out/lib/xfce4/notifyd/xfce4-notifyd $out/bin
+   '';
 
   meta = {
     homepage = http://goodies.xfce.org/projects/applications/xfce4-notifyd;
