04a40e217b28287ba14dabf397214700b9cd0cb5
Vladimír Čunát <vcunat@gmail.com>
2013-01-01 17:57:16 +0100

    Revert "Add dvdstyler and needed wxsvg."
    This reverts commit 19d8b1d9b0f5a66fcb0e7e37388a315fca3f0750.
    
    Conflicts:
    	pkgs/applications/video/dvdstyler/default.nix
    	pkgs/development/libraries/wxsvg/default.nix

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5108,8 +5108,6 @@ let
     withMesa = lib.elem system lib.platforms.mesaPlatforms;
   };
 
-  wxsvg = callPackage ../development/libraries/wxsvg {};
-
   wtk = callPackage ../development/libraries/wtk { };
 
   x264 = callPackage ../development/libraries/x264 { };
@@ -6799,9 +6797,6 @@ let
   dvb_apps  = callPackage ../applications/video/dvb-apps { };
 
   dvdauthor = callPackage ../applications/video/dvdauthor { };
-
-  dvdstyler = callPackage ../applications/video/dvdstyler
-    { inherit (gnome) libgnomeui; };
 
   dvswitch = callPackage ../applications/video/dvswitch { };
 
