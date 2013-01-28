550f5c5993364fc1a56f67897a51ba901f118994
Vladimír Čunát <vcunat@gmail.com>
2013-01-28 18:06:47 +0100

    libpng: minor update

--- pkgs/development/libraries/libpng/libpng-apng.nix
+++ pkgs/development/libraries/libpng/libpng-apng.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchurl, zlib }:
 
 stdenv.mkDerivation rec {
-  version = "1.5.13";
+  version = "1.5.14";
   name = "libpng-apng-${version}";
 
   patch_src = fetchurl {
     url = "mirror://sourceforge/libpng-apng/libpng15/${version}/libpng-${version}-apng.patch.gz";
-    sha256 = "13x39m7wb5prvck0h6g9wxnvc5f9n253yfh8kdncsqjw9pi4ffam";
+    sha256 = "1vcqbkdssy4srm8jqyzaipdc70xzanilqssypmwqyngp8ph0m45p";
   };
 
   src = fetchurl {
     url = "mirror://sourceforge/libpng/libpng-${version}.tar.xz";
-    sha256 = "1vks4mqv4140b10kp53qrywsx9m4xan5ibwsrlmf42ni075zjhxq";
+    sha256 = "0m3vz3gig7s63zanq5b1dgb5ph12qm0cylw4g4fbxlsq3f74hn8l";
   };
 
   preConfigure = ''
