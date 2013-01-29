80a361f4e91582de8845b8af3a8df33a24fe9a92
Vladimír Čunát <vcunat@gmail.com>
2013-01-29 02:31:24 +0100

    major poppler update
    added optional tests, but their compilation fails (probably upstream problem)

--- pkgs/development/libraries/poppler/default.nix
+++ pkgs/development/libraries/poppler/default.nix
@@ -1,20 +1,25 @@
 { fetchurl, stdenv, cairo, freetype, fontconfig, zlib
 , libjpeg, pixman, curl, libpthreadstubs, libXau, libXdmcp, openjpeg
-, libxml2, pkgconfig, cmake, lcms
+, libxml2, pkgconfig, cmake, lcms2
 , gtkSupport ? false, glib ? null, gtk ? null
 , qt4Support ? false, qt4 ? null
+, fetchgit, doCheck?false # some strange test-compilation error
 }:
 
 stdenv.mkDerivation rec {
-  name = "poppler-0.18.4";
+  name = "poppler-0.22.0"; # was 18.4
 
   src = fetchurl {
     url = "${meta.homepage}${name}.tar.gz";
-    sha256 = "0bnl05al7mjndp2h0355946j59nfw76f5v0x57d47q68rm412hik";
+    sha256 = "1rmrspavldlpqi6g76fijcmshy80m0kxd01nc1dmy4id3h4las44";
+  };
+  testdata = fetchgit {
+    url = "git://anongit.freedesktop.org/poppler/test";
+    rev = "0d2bfd4af4c76a3bac27ccaff793d9129df7b57a";
   };
 
   propagatedBuildInputs =
-    [ zlib cairo freetype fontconfig libjpeg lcms pixman curl
+    [ zlib cairo freetype fontconfig libjpeg lcms2 pixman curl
       libpthreadstubs libXau libXdmcp openjpeg libxml2 stdenv.gcc.libc
     ]
     ++ stdenv.lib.optionals gtkSupport [ glib gtk ]
@@ -24,12 +29,22 @@ stdenv.mkDerivation rec {
 
   cmakeFlags = "-DENABLE_XPDF_HEADERS=ON -DENABLE_LIBCURL=ON -DENABLE_ZLIB=ON";
 
+  preConfigure = if !doCheck then "" else "cp -r ${testdata} ../test";
+
   patches = [ ./datadir_env.patch ];
 
-  # XXX: The Poppler/Qt4 test suite refers to non-existent PDF files
-  # such as `../../../test/unittestcases/UseNone.pdf'.
-  #doCheck = !qt4Support;
-  checkTarget = "test";
+  inherit doCheck;
+  checkPhase = ''
+    pushd ../../test
+    #ln -s ../${name}/glib .
+    NIX_CFLAGS="$NIX_CFLAGS -I../${name}"
+    PKG_CONFIG_PATH="../${name}/build:$PKG_CONFIG_PATH"
+    chmod +rw Makefile
+    echo "CFLAGS += -I../${name}/glib -I../${name}/build/glib" >> Makefile
+    make # build the testers
+    popd
+    make test
+  '';
 
   enableParallelBuilding = true;
 
