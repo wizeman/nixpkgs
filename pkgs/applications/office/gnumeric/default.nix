96af4ec4451dfa8a4d8c96c59a817d1509d7af10
Vladimír Čunát <vcunat@gmail.com>
2013-02-05 22:40:45 +0100

    gnumeric: update

--- pkgs/applications/office/gnumeric/default.nix
+++ pkgs/applications/office/gnumeric/default.nix
@@ -1,21 +1,25 @@
-{ stdenv, fetchurl
-, bzip2, glib, goffice, gtk3, intltool, libglade, libgsf, libxml2
-, pango, pkgconfig, scrollkeeper, zlib
+{ stdenv, fetchurl, pkgconfig, intltool, perl, perlXMLParser
+, goffice, gtk3
 }:
 
-stdenv.mkDerivation {
-  name = "gnumeric-1.11.3";
+stdenv.mkDerivation rec {
+  name = "gnumeric-1.12.0";
 
   src = fetchurl {
-    url = mirror://gnome/sources/gnumeric/1.11/gnumeric-1.11.3.tar.xz;
-    sha256 = "1hblcbba4qzlby094dih6ncclgf2n5ac59lqg9dykpz8ad3hxw72";
+    url = "mirror://gnome/sources/gnumeric/1.12/${name}.tar.xz";
+    sha256 = "04a1w38abx25cbix8diy6ihf4f3q3vxw9a7x595lblg517cm6yq3";
   };
+
+  preConfigure = ''sed -i 's/\(SUBDIRS.*\) doc/\1/' Makefile.in ''; # fails when installing docs
 
   configureFlags = "--disable-component";
 
+  # ToDo: optional libgda, python, introspection?
   buildInputs = [
-    bzip2 glib goffice gtk3 intltool libglade libgsf libxml2
-    pango pkgconfig scrollkeeper zlib
+    #bzip2 glib goffice gtk3 intltool libglade libgsf libxml2
+    #pango pkgconfig scrollkeeper zlib
+    pkgconfig intltool perl perlXMLParser
+    goffice gtk3
   ];
 
   meta = {
