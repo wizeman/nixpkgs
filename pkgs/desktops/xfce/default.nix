6bcd56fc8b4c500cfc40d05da2263abb5bb093ad
Vladimír Čunát <vcunat@gmail.com>
2013-03-02 09:41:53 +0100

    xfce: NixOS support

--- pkgs/desktops/xfce/default.nix
+++ pkgs/desktops/xfce/default.nix
@@ -17,6 +17,8 @@ deps = rec { # xfce-global dependency overrides should be here
 
 xfce_self = rec { # the lines are very long but it seems better than the even-odd line approach
 
+  inherit (deps) gvfs; # used by NixOS
+
   #### CORE                 from "mirror://xfce/src/xfce/${p_name}/${ver_maj}/${name}.tar.bz2"
 
   exo             = callPackage ./core/exo.nix { };
