d4a0ddfd0968a36c8a5138d381d1dc9ae215ca08
Vladimír Čunát <vcunat@gmail.com>
2013-04-25 20:11:47 +0200

    attempt: the trunk head should have fixed this now

--- pkgs/applications/display-managers/lightdm/default.nix
+++ pkgs/applications/display-managers/lightdm/default.nix
@@ -1,22 +1,35 @@
-{ stdenv, fetchurl, pam, pkgconfig, libxcb, glib, libXdmcp, itstool, libxml2, intltool, x11, libxklavier, libgcrypt, makeWrapper }:
+{ stdenv, pam, pkgconfig, libxcb, glib, libXdmcp, itstool, libxml2, intltool, x11, libxklavier, libgcrypt, makeWrapper
+, fetchbzr, autoconf, automake, libtool, m4, gobjectIntrospection, gnome }:
 
-stdenv.mkDerivation {
-  name = "lightdm-1.5.1";
+let
+  #ver_maj = "1.6";
+  #ver_min = "0";
+  #version = "${ver_maj}.${ver_min}";
+  ver_bzr = 1656;
+  version = "bzr";
+in
+stdenv.mkDerivation rec {
+  name = "lightdm-${version}";
 
-  src = fetchurl {
-    url = https://launchpad.net/lightdm/1.6/1.5.1/+download/lightdm-1.5.1.tar.xz;
-    sha256 = "645db2d763cc514d6aecb1838f4a9c33c3dcf0c94567a7ef36c6b23d8aa56c86";
+  #src = fetchurl {
+  #  url = "${meta.homepage}/${ver_maj}/${version}/+download/${name}.tar.xz";
+
+  src = fetchbzr {
+    url = "http://bazaar.launchpad.net/~lightdm-team/lightdm/trunk";
+    revision = ver_bzr;
+    sha256 = "0n8mj6gr64rcqza28149jl7a32rfg18z5x4wzbm5xil0xa6q2wa5";
   };
 
-  buildInputs = [ pkgconfig pam libxcb glib libXdmcp itstool libxml2 intltool libxklavier libgcrypt makeWrapper ];
-
-  configureFlags = [ "--enable-liblightdm-gobject" ];
-
-  patches =
-    [ ./lightdm.patch
-    ];
-
+  patches = [ ./lightdm.patch ];
   patchFlags = "-p0";
+
+  buildInputs = [
+    pkgconfig pam libxcb glib libXdmcp itstool libxml2 intltool libxklavier libgcrypt makeWrapper
+    autoconf automake libtool m4 gobjectIntrospection gnome.gtkdoc
+  ];
+
+  postPatch = "sed '/^SUBDIRS/s/doc help//' -i Makefile.am"; # some problems there
+  configurePhase = "./autogen.sh --enable-liblightdm-gobject --prefix=$out";
 
   meta = {
     homepage = http://launchpad.net/lightdm;
