bbddf9245d5addeacebe92d2af155fd40d6327ab
Vladimír Čunát <vcunat@gmail.com>
2013-06-20 11:09:21 +0200

    llvm: update .2->.3, really use libffi, enable R600

--- pkgs/development/compilers/llvm/default.nix
+++ pkgs/development/compilers/llvm/default.nix
@@ -24,6 +24,7 @@ stdenv.mkDerivation rec {
   cmakeFlags = with stdenv; [
     "-DCMAKE_BUILD_TYPE=Release"
     "-DLLVM_ENABLE_FFI=ON"
+    "-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=R600" # for mesa
     "-DLLVM_BINUTILS_INCDIR=${binutils_gold}/include"
   ] ++ lib.optional (!isDarwin) [ "-DBUILD_SHARED_LIBS=ON" ];
 
