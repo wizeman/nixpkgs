0c517827cf16bc409f0cf3e8417fd1d0b4be3713
Vladimír Čunát <vcunat@gmail.com>
2013-07-28 12:26:54 +0200

    switch back to gtk3 by default
    It seems better supported, moreover I could not make the icons work with gtk2.

--- pkgs/applications/display-managers/lightdm-gtk-greeter/default.nix
+++ pkgs/applications/display-managers/lightdm-gtk-greeter/default.nix
@@ -1,10 +1,14 @@
 { stdenv, fetchurl, lightdm, pkgconfig, intltool
-, useGTK2 ? true, gtk2, gtk3 # don't pull gtk3 by default
+, hicolor_icon_theme, makeWrapper
+, useGTK2 ? false, gtk2, gtk3 # gtk3 seems better supported
 }:
+
+#ToDo: bad icons with gtk2;
+#  avatar icon is missing in standard hicolor theme, I don't know where gtk3 takes it from
 
 let
   ver_branch = "1.6";
-  version = "1.5.1"; # 1.5.2 results into infinite cycling of X in restarts
+  version = "1.5.1"; # 1.5.2 and 1.6.0 result into infinite cycling of X in restarts
 in
 stdenv.mkDerivation rec {
   name = "lightdm-gtk-greeter-${version}";
@@ -17,13 +21,17 @@ stdenv.mkDerivation rec {
   patches = [ ./lightdm-gtk-greeter.patch ];
   patchFlags = "-p0";
 
-  buildInputs = [ pkgconfig lightdm intltool (if useGTK2 then gtk2 else gtk3) ];
+  buildInputs = [ pkgconfig lightdm intltool ]
+    ++ (if useGTK2 then [ gtk2 makeWrapper ] else [ gtk3 ]);
 
   configureFlags = stdenv.lib.optional useGTK2 "--with-gtk2";
 
   postInstall = ''
       substituteInPlace "$out/share/xgreeters/lightdm-gtk-greeter.desktop" \
         --replace "Exec=lightdm-gtk-greeter" "Exec=$out/sbin/lightdm-gtk-greeter"
+    '' + stdenv.lib.optionalString useGTK2 ''
+      wrapProgram "$out/sbin/lightdm-gtk-greeter" \
+        --prefix XDG_DATA_DIRS ":" "${hicolor_icon_theme}/share"
     '';
 
   meta = {
