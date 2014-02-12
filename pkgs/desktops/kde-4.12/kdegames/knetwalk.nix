33d6ac498a34554a944dff835c8fe7e7b0ca043b
Ricardo M. Correia <rcorreia@wizy.org>
2014-02-12 03:57:31 +0100

    knetwalk: Add "Clear locks" keyboard shortcut

--- pkgs/desktops/kde-4.12/kdegames/knetwalk.nix
+++ pkgs/desktops/kde-4.12/kdegames/knetwalk.nix
@@ -1,6 +1,7 @@
 { kde, kdelibs, libkdegames }:
 kde {
   buildInputs = [ kdelibs libkdegames ];
+  patches = [ ./knetwalk-clearlocks.patch ];
   meta = {
     description = "a small game where you have to build up a computer network by rotating the wires to connect the terminals to the server";
   };
