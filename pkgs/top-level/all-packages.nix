261f5a959e6000e5260f6c841d51db9dc5583f37
Vladimír Čunát <vcunat@gmail.com>
2014-02-27 08:22:41 +0100

    texinfo: move ncurses support into texinfoInteractive
    This, for example, drops the stdenv dependency on ncurses.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3781,6 +3781,9 @@ let
   texinfo5 = callPackage ../development/tools/misc/texinfo/5.2.nix { };
   texinfo4 = texinfo413;
   texinfo = texinfo5;
+  texinfoInteractive = appendToName "interactive" (
+    texinfo.override { interactive = true; }
+  );
 
   texi2html = callPackage ../development/tools/misc/texi2html { };
 
