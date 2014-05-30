58fca8929d49bf27ceefb72d220c6f5cbeabea44
Ricardo M. Correia <rcorreia@wizy.org>
2014-05-30 23:10:00 +0200

    nixos/release-combined.nix: Disable OVA test

--- nixos/release-combined.nix
+++ nixos/release-combined.nix
@@ -54,7 +54,7 @@ in rec {
 
         (all nixos.iso_minimal)
         nixos.iso_graphical.x86_64-linux
-        nixos.ova.x86_64-linux
+        #nixos.ova.x86_64-linux
 
         #(all nixos.tests.containers)
         nixos.tests.chromium
