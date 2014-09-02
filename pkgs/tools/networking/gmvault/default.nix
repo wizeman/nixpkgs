f3b591226058862d9b4d9420fc85ee81503aa5e8
Ricardo M. Correia <rcorreia@wizy.org>
2014-09-02 17:25:06 +0200

    gmvault: Update to latest git commit
    Fixes authentication with gmail, as well as many other bugs.

--- pkgs/tools/networking/gmvault/default.nix
+++ pkgs/tools/networking/gmvault/default.nix
@@ -1,30 +1,27 @@
-{ pkgs, fetchurl, buildPythonPackage, pythonPackages }:
+{ pkgs, fetchFromGitHub, buildPythonPackage, pythonPackages }:
 
 buildPythonPackage rec {
-  version = "1.8.1-beta";
+  version = "1.9.1-git-2015-08-06";
   name = "gmvault-${version}";
 
-  src = fetchurl {
-    url = "https://bitbucket.org/gaubert/gmvault-official-download/downloads/gmvault-v${version}-src.tar.gz";
-    name = "${name}.tar.bz";
-    sha256 = "0b575cnrd6jzcpa05mbn2swzcy0r9ck09pkhs4ydj6d3ir52j80c";
+  src = fetchFromGitHub {
+    owner = "gaubert";
+    repo = "gmvault";
+    rev = "4de847437a9ac29368d2002e8fa8c7f3630dc948";
+    sha256 = "16nkpwxaj1rigjqxd159l7ss2p8rywl2vd0cj852ni4vk5n9k2p2";
   };
 
   doCheck = false;
 
-  propagatedBuildInputs = [
-    pythonPackages.gdata
-    pythonPackages.IMAPClient
-    pythonPackages.Logbook
-    pythonPackages.argparse
+  propagatedBuildInputs = with pythonPackages; [
+    argparse Logbook IMAPClient chardet
   ];
 
   startScript = ./gmvault.py;
 
-  patchPhase = ''
+  postPatch = ''
     cat ${startScript} > etc/scripts/gmvault
     chmod +x etc/scripts/gmvault
-    substituteInPlace setup.py --replace "Logbook==0.4.1" "Logbook==0.4.2"
   '';
 
   meta = {
