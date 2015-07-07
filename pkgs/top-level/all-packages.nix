dc688cc9bbb2856bb2a53e4e2cc37c17872f0f8c
Robert Irelan <rirelan@gmail.com>
2015-07-07 08:29:08 -0700

    cargoSnapshot: 2015-04-02 -> 2015-06-17, hide from nix-env
    Update snapshot to avoid rust-lang/cargo#976, which otherwise breaks the
    build.
    
    Also move the `cargoSnapshot` derivation inside a set in
    pkgs/top-level/all-packages.nix in order to hide the `cargo-snapshot`
    packages from `nix-env -qa`, since it's only used to build the `cargo`
    package.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4657,7 +4657,7 @@ let
   rustUnstable = recurseIntoAttrs (makeRustPlatform rustcMaster cargo rustUnstable);
 
   # rust platform to build cargo itself (with cargoSnapshot)
-  rustCargoPlatform = makeRustPlatform rustc cargoSnapshot rustCargoPlatform;
+  rustCargoPlatform = makeRustPlatform rustc cargoSnapshot.cargo rustCargoPlatform;
 
   makeRustPlatform = rustc: cargo: self:
     let
@@ -5311,7 +5311,9 @@ let
     rustPlatform = rustCargoPlatform;
   };
 
-  cargoSnapshot = callPackage ../development/tools/build-managers/cargo/snapshot.nix { };
+  cargoSnapshot = {
+    cargo = callPackage ../development/tools/build-managers/cargo/snapshot.nix { };
+  };
 
   casperjs = callPackage ../development/tools/casperjs { };
 
