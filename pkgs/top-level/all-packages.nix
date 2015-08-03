503139e0c787bbc02a081700810c913c134aba63
Ricardo M. Correia <rcorreia@wizy.org>
2015-08-03 17:23:07 +0200

    buildLinux: make it overridable
    ... so that we can override which compiler gets used to build the kernel.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9974,7 +9974,7 @@ let
 
   # A function to build a manually-configured kernel
   linuxManualConfig = pkgs.buildLinux;
-  buildLinux = import ../os-specific/linux/kernel/manual-config.nix {
+  buildLinux = makeOverridable (import ../os-specific/linux/kernel/manual-config.nix) {
     inherit (pkgs) stdenv runCommand nettools bc perl kmod writeTextFile ubootChooser;
   };
 
